bee8a352dcb8761be4adfe8033679aa2452821a3
unknown <zachb89@gmail.com>
2021-01-02 18:03:26 -0600

    Initial commit.

---
+++ BindingToAnObjectExample/BindingToAnObjectExample/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingToAnObjectExample
+{
+   public class Employee
+    {
+        public string EmployeeName { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return EmployeeName;
+        //}
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+https://learning.oreilly.com/videos/intermediate-windows-presentation/9781491955017/
+
+https://resources.oreilly.com/examples/0636920049937
